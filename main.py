18d354a942b2e8199049a591a319bae6e0fc0c20
Valentin DEBENATH <valentin.debenath@gmail.com>
2017-09-26 22:41:19 +0200

    Ajout d'une fonction de visualisation 3D du perlin noise

--- main.py
+++ main.py
@@ -1,10 +1,44 @@
 from perlinnoise import *
 from gradient import *
+from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+import numpy as np
 
 np.random.seed()
 pn = makePerlinNoise(800,600,8,1.0,0.5)
-img = applyGradient(pn,blackAndWhiteGradient)
-imgplot = plt.imshow(img)
-plt.show()
+
+
+def plot2D() :
+    img = applyGradient(pn,blackAndWhiteGradient)
+    imgplot = plt.imshow(img)
+    plt.show()
+
+def plot3D():
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+
+    # Make data.
+    Y = np.arange(0, 800, 1)
+    X = np.arange(0, 600, 1)
+    X, Y = np.meshgrid(X, Y)
+
+    Z=pn
+
+    # Plot the surface.
+    surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,
+                           linewidth=0, antialiased=False)
+
+    # Customize the z axis.
+    ax.set_zlim(-1.01, 1.01)
+    ax.zaxis.set_major_locator(LinearLocator(10))
+    ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+
+    # Add a color bar which maps values to colors.
+    fig.colorbar(surf, shrink=0.5, aspect=5)
+
+    plt.show()
+
+plot2D()
+plot3D()
